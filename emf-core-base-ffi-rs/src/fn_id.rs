bc256800c6fd3734ef276a33a66839ac859ecd4c
Gabriel Borrelli <gabriel.borrelli@nanoshellsoft.com>
2020-12-27 11:59:27 +0100

    Add the function id's for the sys api

--- emf-core-base-ffi-rs/src/fn_id.rs
+++ emf-core-base-ffi-rs/src/fn_id.rs
@@ -7,4 +7,12 @@
 #[non_exhaustive]
 pub enum FnId {
     SysLock = 1,
+    SysTryLock = 2,
+    SysUnlock = 3,
+    SysShutdown = 4,
+    SysPanic = 5,
+    SysHasFunction = 6,
+    SysGetFunction = 7,
+    SysGetSyncHandler = 8,
+    SysSetSyncHandler = 9,
 }
