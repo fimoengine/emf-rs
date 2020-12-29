1edff00e6f6df73d51a2addd0457b838106271c8
Gabriel Borrelli <gabriel.borrelli@nanoshellsoft.com>
2020-12-29 22:42:33 +0100

    Add the BaseInterface struct

--- emf-core-base-ffi-rs/src/lib.rs
+++ emf-core-base-ffi-rs/src/lib.rs
@@ -1,5 +1,6 @@
 mod boolean;
 mod fn_id;
+mod interface;
 
 pub mod containers;
 pub mod fn_ptr;
@@ -10,17 +11,10 @@ pub mod version;
 
 pub use boolean::Bool;
 pub use fn_id::FnId;
+pub use interface::BaseInterface;
 
 /// An opaque structure representing the emf-core-base interface.
 #[repr(C)]
 pub struct BaseT {
     _private: [u8; 0],
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
