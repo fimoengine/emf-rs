659b2b4d2819491ef6e9d5a2a17ff3294d36dba6
Gabriel Borrelli <gabriel.borrelli@nanoshellsoft.com>
2021-01-01 11:28:50 +0100

    Add the Debug trait to Bool

--- emf-core-base-rs-ffi-bare/src/boolean.rs
+++ emf-core-base-rs-ffi-bare/src/boolean.rs
@@ -4,7 +4,7 @@
 /// An enum describing a boolean value.
 // TODO: Replace with `#[repr(i8)]` once https://github.com/rust-lang/rust/issues/80556 is fixed.
 #[repr(u8)]
-#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
 pub enum Bool {
     False = 0,
     True = 1,
