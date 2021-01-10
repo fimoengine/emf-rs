38f5fd85bbd6539070d46517ae1ea2479a94fa7f
Gabriel Borrelli <gabriel.borrelli@nanoshellsoft.com>
2021-01-10 16:49:00 +0100

    Implement the binding

--- emf-core-base-rs/src/lib.rs
+++ emf-core-base-rs/src/lib.rs
@@ -1,7 +1,9 @@
-pub use emf_core_base_rs_bare;
+//! Idiomatic rust bindings for the `emf-core-base` interface.
+//!
+//! This crate provides idiomatic wrappers of the function and type definitions specified by the
+//! [emf-core-base](https://fimoengine.github.io/emf/emf-core-base/index.html) interface.
+//! No implementation for those functions is provided.
 
-pub mod ffi_bindings {
-    #[cfg(feature = "global_api")]
-    pub use emf_core_base_rs_ffi::initialize_base_binding;
-    pub use emf_core_base_rs_ffi::InterfaceLoader;
-}
+pub use emf_core_base_rs_bare::*;
+
+pub mod bindings;
