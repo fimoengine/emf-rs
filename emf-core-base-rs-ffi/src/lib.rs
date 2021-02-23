a8d45ada576164f36ef292ebc783b0a372cd9585
Gabriel Borrelli <gabriel.borrelli@nanoshellsoft.com>
2021-02-23 17:18:01 +0100

    Remove previous

--- emf-core-base-rs-ffi/src/lib.rs
+++ emf-core-base-rs-ffi/src/lib.rs
@@ -1,16 +1,7 @@
-//! Rust bindings for the `emf-core-base` interface.
-//!
-//! This crate provides the function and type definitions specified by the
-//! [emf-core-base](https://fimoengine.github.io/emf/emf-core-base/index.html) interface.
-//! No implementation for those functions is provided.
-
-#![feature(const_fn_transmute)]
-#![feature(const_mut_refs)]
-
-#[cfg(feature = "global_api")]
-pub use bindings::initialize_base_binding;
-pub use emf_core_base_rs_ffi_bare::*;
-pub use interface_loader::InterfaceLoader;
-
-mod bindings;
-mod interface_loader;
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- emf-core-base-rs/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Idiomatic rust bindings for the `emf-core-base` interface.
-//!
-//! This crate provides idiomatic wrappers of the function and type definitions specified by the
-//! [emf-core-base](https://fimoengine.github.io/emf/emf-core-base/index.html) interface.
-//! No implementation for those functions is provided.
-
-pub use emf_core_base_rs_bare::*;
-
-pub mod bindings;
